c09f5ef511b38ec529ea2149bd539d3fcf870afa
WinSpoon <winstone851108@gmail.com>
2022-06-23 19:32:07 +0900

    Framework v0.2

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -1,59 +1,30 @@
-// ** Framework v0.1
+// ** Framework v0.2
 #include <iostream>
 
-
 // ** namespace 
-// ** 생성자 & 소멸자 & 복사생성자 
-// ** 가상함수 & 순수가상함수
-// ** 오버로딩 & 오버라이딩
-// ** 연산자 오버로딩
-
-
-
-struct Info
-{
-private:
-	int iNumber;
-};
-
-class Object
-{
-// ** 비공개 형태로 만들어 줌.
-private:
-	int iNumber;
-
-// ** 상속간의 접근이 가능.
-protected:
-
-// ** 공개된 형태로 만들어 줌.
-public:
-	int zzz;
-
-public:
-	void Setter(int _Number) { iNumber = _Number; }
-	int Getter() { return iNumber; }
-};
 
 /*
-class Object
-{
-
-
-};
+using std::cout;
+using std::endl;
 */
 
+using namespace std;
+
+// ** 생성자 & 소멸자 & 복사생성자 
+
+// ** 가상함수 & 순수가상함수
+
+// ** 오버로딩 & 오버라이딩
+
+// ** 연산자 오버로딩
 
 
 int main(void)
 {
-	Object o1, o2;
-	
-	o1.Setter(10);
-	o2.Setter(20);
+	cout << "Hello World!!" << endl;
 
-	std::cout << o1.Getter() << std::endl;
-	std::cout << o2.Getter() << std::endl;
-	
 
 	return 0;
 }
+
+
