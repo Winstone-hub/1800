cfc4c56e1ad7fc2f08333c201500e1a6df1e7c54
WinSpoon <winstone851108@gmail.com>
2022-06-27 19:56:21 +0900

    Framework v0.8 (Operator)

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -1,106 +1,153 @@
-// ** Framework v0.7
+// ** Framework v0.8
 #include <iostream>
 #include <Windows.h>
 
 using namespace std;
 
+
+// ** [&] 사용 용도
+// ** 1. 2항 연산자로 사용
+// **	- & <- 1번만 쓰였다면 [비트연산]
+// **	- && <- 2번 쓰였다면 [비교연산 & 논리연산]
+
+// ** 2. 단항 연산자로 사용
+// **	- &변수 <- 변수 앞쪽에 붙은경우 [주소반환 연산자]
+// **	- 자료형(형태)& <- 자료현 뒷쪽에 붙은 경우 [레퍼런스 연산자] 
+
+/*
+int Add(const int& _i)
+{
+	return _i + 1;
+}
+
+int main(void)
+{
+	int i = 10;
+	int n = Add(i);
+	cout << n << endl;
+
+	return 0;
+}
+*/
+
+
+
+
+
+
 // ** 연산자 오버로딩
+// ** Operator
 
-
-// ** 상속 & 다형성
-// ** 가상함수 & 순수가상함수
-struct Vector3
+/*
+class Object
 {
-	float x, y, z;
+private:
+	int Number;
 
-	Vector3() {}
-
-	Vector3(float _x, float _y) : x(_x), y(_y), z(0) {}
-
-	Vector3(float _x, float _y, float _z)
-		: x(_x), y(_y), z(_z) {}
-};
-
-// ** 오버로딩 & 오버라이딩
-
-class Parent // 추상 클래스
-{
-protected:
-	string Name;
-public://								관계 : 오버로딩
-	virtual void Output()PURE;	// <─────────────────┐ 
-	virtual void Output(string _str)PURE; // <───────┘   <────────────────────────────────────────────────┐
-//																									      │
-	Parent() {}//																						  │
-	Parent(string _str) : Name(_str) {}//																  │
-};//																									  │
-//																							   [관계 : 오버 라이딩]
-class Child : public Parent//																			  │
-{//																										  │
-public://																	관계 : 오버로딩				  │
-	virtual void Output() override { cout << "Child : " << Name << endl; }	// <─────────────────┐ 		  │
-	virtual void Output(string _str) override { cout << "Child : " << Name << endl; } // <───────┘   <────┘
-	void Input() {  }
 public:
-	Child() {}
-	Child(string _str) : Parent(_str) {}
-};
+	int GetNumber() { return Number; }
 
 
-class A : public Child
-{
-public:
-	virtual void Output() override { cout << "A : " << Name << endl; }
-	virtual void Output(string _str) override { cout << "A : " << Name << endl; }
-public:
-	A() {}
-	A(string _str) : Child(_str) {}
-};
+	Object& operator+=(Object& _obj)
+	{
+		this->Number += _obj.Number;
+		return (*this);
+	}
+	Object& operator+=(int _number)
+	{
+		this->Number += _number;
+		return (*this);
+	}
 
-
-
-
-
-
-
-class Object : public Parent
-{
-public:
-	virtual void Output() { cout << "Object : " << Name << endl; }
-	virtual void Output(string _str) override { cout << "Object : " << Name << endl; }
+	Object operator+(Object _Obj)
+	{
+		return Object(this->Number + _Obj.Number);
+	}
+	
 public:
 	Object() {}
-	Object(string _str) : Parent(_str) {}
+	Object(int _number) : Number(_number) { }
+	~Object() {}
 };
+*/
 
 
 
 
 
-const int ObjectID_Child	= 0;
-const int ObjectID_Object	= 1;
-const int ObjectID_A		= 2;
-const int ObjectID_MAX		= 3;
+
+/*
+class Vector3
+{
+private:
+	float x, y, z;
+public:
+	void Output()
+	{
+		cout << x << ", " << y << ", " << z << endl;
+	}
+	Vector3 operator+(Vector3 _v1)
+	{
+		return Vector3(
+			this->x + _v1.x, 
+			this->y + _v1.y, 
+			this->z + _v1.z);
+	}
+public:
+	Vector3() {}
+	Vector3(float _x, float _y, float _z) 
+		: x(_x) , y(_y) , z(_z) { }
+	~Vector3() {}
+};
+*/
+
+
 
 
 int main(void)
 {
-	Parent* p[ObjectID_MAX][128];
+	/*
+	int i1(1), i2(2);
+	i1 += i2;
+	i1 += 7;
+	cout << i1 << endl;
+	*/
 
-	for (int i = 0; i < ObjectID_MAX; ++i)
-	{
-		for (int j = 0; j < 128; ++j)
-		{
-			p[i][j]->Output();
-		}
-	}
 
-	for (int j = 0; j < 128; ++j)
-	{
-		p[ObjectID_Child][j]->Output();
-		p[ObjectID_Object][j]->Output();
-		p[ObjectID_A][j]->Output();
-	}
+
+
+
 	
+
+	/*
+	Object o1(1), o2(2);
+	//o1 += o2;
+	//o1 += 7;
+	//++o1;
+
+	Object o3 = o1 + o2;
+
+	cout << o3.GetNumber() << endl;
+	*/
+
+
+
+
+
+
+
+
+
+	/*
+	Vector3 V1, V2;
+
+	V1 = Vector3(10, 20, 30);
+	V2 = Vector3(100, 200, 300);
+
+	Vector3 V3 = V1 + V2;
+
+	V3.Output();
+	*/
+
 	return 0;
 }
