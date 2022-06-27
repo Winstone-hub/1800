1dde8eb04c373806f60ef2e4315945080cc50a95
WinSpoon <winstone851108@gmail.com>
2022-06-27 20:51:11 +0900

    Framework v1.0  GetTickCount

--- Framework/Framework/Main.cpp
+++ Framework/Framework/Main.cpp
@@ -1,153 +1,37 @@
-// ** Framework v0.8
+// ** Framework v1.0  GetTickCount64
 #include <iostream>
 #include <Windows.h>
 
 using namespace std;
 
 
-// ** [&] 사용 용도
-// ** 1. 2항 연산자로 사용
-// **	- & <- 1번만 쓰였다면 [비트연산]
-// **	- && <- 2번 쓰였다면 [비교연산 & 논리연산]
-
-// ** 2. 단항 연산자로 사용
-// **	- &변수 <- 변수 앞쪽에 붙은경우 [주소반환 연산자]
-// **	- 자료형(형태)& <- 자료현 뒷쪽에 붙은 경우 [레퍼런스 연산자] 
-
-/*
-int Add(const int& _i)
-{
-	return _i + 1;
-}
+// 게임 
 
 int main(void)
 {
-	int i = 10;
-	int n = Add(i);
-	cout << n << endl;
+	// ** DWORD unsigned long
+	// ** ULONGLONG unsigned __int64
 
-	return 0;
-}
-*/
+	//					1 / 1000     max 시간 = 49일
+	DWORD dwTime = GetTickCount();
 
+	//					1 / 1000     max 시간 = 500년
+	ULONGLONG Time = GetTickCount64();
 
+	int i = 0;
 
-
-
-
-// ** 연산자 오버로딩
-// ** Operator
-
-/*
-class Object
-{
-private:
-	int Number;
-
-public:
-	int GetNumber() { return Number; }
-
-
-	Object& operator+=(Object& _obj)
+	while (true)
 	{
-		this->Number += _obj.Number;
-		return (*this);
-	}
-	Object& operator+=(int _number)
-	{
-		this->Number += _number;
-		return (*this);
+		if (Time + 1000 <= GetTickCount64())
+		{
+			Time = GetTickCount64();
+
+			system("cls");
+
+			cout << ++i << endl;
+		}
 	}
 
-	Object operator+(Object _Obj)
-	{
-		return Object(this->Number + _Obj.Number);
-	}
-	
-public:
-	Object() {}
-	Object(int _number) : Number(_number) { }
-	~Object() {}
-};
-*/
-
-
-
-
-
-
-/*
-class Vector3
-{
-private:
-	float x, y, z;
-public:
-	void Output()
-	{
-		cout << x << ", " << y << ", " << z << endl;
-	}
-	Vector3 operator+(Vector3 _v1)
-	{
-		return Vector3(
-			this->x + _v1.x, 
-			this->y + _v1.y, 
-			this->z + _v1.z);
-	}
-public:
-	Vector3() {}
-	Vector3(float _x, float _y, float _z) 
-		: x(_x) , y(_y) , z(_z) { }
-	~Vector3() {}
-};
-*/
-
-
-
-
-int main(void)
-{
-	/*
-	int i1(1), i2(2);
-	i1 += i2;
-	i1 += 7;
-	cout << i1 << endl;
-	*/
-
-
-
-
-
-	
-
-	/*
-	Object o1(1), o2(2);
-	//o1 += o2;
-	//o1 += 7;
-	//++o1;
-
-	Object o3 = o1 + o2;
-
-	cout << o3.GetNumber() << endl;
-	*/
-
-
-
-
-
-
-
-
-
-	/*
-	Vector3 V1, V2;
-
-	V1 = Vector3(10, 20, 30);
-	V2 = Vector3(100, 200, 300);
-
-	Vector3 V3 = V1 + V2;
-
-	V3.Output();
-	*/
 
 	return 0;
 }
