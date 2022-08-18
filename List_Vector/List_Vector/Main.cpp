5874b95a7ba6badf46fd9f845bfef99599cf6ae2
WinSpoon <winstone1108@naver.com>
2022-08-18 20:10:55 +0900

    vector Function

--- List_Vector/List_Vector/Main.cpp
+++ List_Vector/List_Vector/Main.cpp
@@ -77,10 +77,14 @@ int main(void)
 	}
 	*/
 
+
+
+	//=====================================================
 	//=====================================================
 
-	// ** Function
 
+
+	// ** List Function
 	{
 		list<int> Numbers;
 
@@ -108,7 +112,7 @@ int main(void)
 		// ** _Where 위치에 value 값을 포함시킨다.
 		// ** _Where 은 반복자
 		//Numbers.insert(_Where, value);
-		
+
 
 		// ** 반복자가 있는 위치에 원소를 삭제한다. 그리고 다음 반복자를 반환한다.
 		//iter = Numbers.erase(iter);
@@ -127,7 +131,7 @@ int main(void)
 		//Numbers.clear();
 
 
-		
+
 
 
 		// ** 현재 노드의 총 개수를 반환한다.
@@ -143,7 +147,7 @@ int main(void)
 			{
 				cout << "False : 데이터가 삽입 됩니다." << endl;
 				Numbers.push_back(10);
-			}	
+			}
 			else
 			{
 				cout << "True : 출력" << endl;
@@ -155,7 +159,7 @@ int main(void)
 			cout << endl;
 		}
 		*/
-		
+
 
 		/*
 		// ** 리스트를 정렬한다.
@@ -165,7 +169,7 @@ int main(void)
 				srand(GetTickCount64() * (i + 1) * GetTickCount64());
 				Numbers.push_back(rand()%100 + 1);
 			}
-			
+
 			cout << "[정렬전]" << endl;
 			for (list<int>::iterator iter = Numbers.begin()
 				; iter != Numbers.end(); ++iter)
@@ -178,6 +182,128 @@ int main(void)
 			for (list<int>::iterator iter = Numbers.begin()
 				; iter != Numbers.end(); ++iter)
 				cout << *iter << endl;
+			cout << endl;
+		}
+		*/
+	}
+
+	
+
+	//=====================================================
+	//=====================================================
+
+
+
+	// ** vector Function
+	{
+		vector<int> Numbers;
+
+		// ** 첫번째 원소를 가르키는 반복자
+		//Numbers.begin();
+
+		// ** 첫번째 원소를 반환한다.
+		//Numbers.front();
+
+		// ** 마지막을 가르키는 반복자
+		//Numbers.end();
+
+		// ** 마지막 원소를 반환한다.
+		//Numbers.back();
+
+
+		// ** 마지막 위치에 값을 추가한다.
+		//Numbers.push_back();
+
+
+		// ** _Where 위치에 value 값을 포함시킨다.
+		// ** _Where 은 반복자
+		//Numbers.insert(_Where, value);
+
+
+		// ** 반복자가 있는 위치에 원소를 삭제한다. 그리고 다음 반복자를 반환한다.
+		//iter = Numbers.erase(iter);
+
+		// ** 마지막 원소 삭제한다.
+		//Numbers.pop_back();
+
+		// ** 전체 원소를 삭제한다.
+		//Numbers.clear();
+
+
+
+
+
+		// ** 현재 노드의 총 개수를 반환한다.
+		//Numbers.size();
+
+		// ** 노드가 삽입될 수 있는 여유 공간의 수.
+		//Numbers.capacity();
+
+		// ** 배열처럼 접근 가능
+		//Numbers[index]
+
+
+		/*
+		// int 
+		{
+			for (int i = 0; i < 10; ++i)
+				Numbers.push_back(i * 10 + 10);
+
+			for (int i = 0; i < 10; ++i)
+				cout << Numbers[i] << endl;
+		}
+		cout << endl;
+		*/
+
+
+		/*
+		// size_t
+		{
+			for (int i = 0; i < 10; ++i)
+				Numbers.push_back(i * 10 + 10);
+
+			for (size_t i = 0; i < Numbers.size(); ++i)
+				cout << Numbers[i] << endl;
+		}
+		cout << endl;
+		*/
+
+
+		/*
+		// iterator
+		{
+			for (int i = 0; i < 10; ++i)
+				Numbers.push_back(i * 10 + 10);
+
+			for (vector<int>::iterator iter = Numbers.begin()
+				; iter != Numbers.end(); ++iter)
+				cout << *iter << endl;
+		}
+		cout << endl;
+		*/
+
+
+
+
+
+		// ** 리스트에 값이 있는지 없는지 확인한다.
+		//Numbers.empty();
+
+		/*
+		for (int i = 0; i < 2; ++i)
+		{
+			if (Numbers.empty())
+			{
+				cout << "False : 데이터가 삽입 됩니다." << endl;
+				Numbers.push_back(10);
+			}
+			else
+			{
+				cout << "True : 출력" << endl;
+				for (vector<int>::iterator iter = Numbers.begin()
+					; iter != Numbers.end(); ++iter)
+					cout << *iter << endl;
+			}
 			cout << endl;
 		}
 		*/
